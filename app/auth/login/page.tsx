36f78ae6e2b78c9396607f3671595a86d3544cfe
Sergi Rojas Navarro <s.waterpolin@gmail.com>
2026-08-25 01:38:40 +0200

    sop

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -12,7 +12,7 @@ import { useRouter } from "next/navigation"
 import { useState } from "react"
 import Image from "next/image"
 
-export default function LoginPage() {
+export default function LoginPage() { 
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [error, setError] = useState<string | null>(null)
